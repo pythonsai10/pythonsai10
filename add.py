5991256e3be5381e3dfac7b28ff88f071d097156
pythonsai10 <43162447+pythonsai10@users.noreply.github.com>
2018-09-11 10:32:02 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,4 @@
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+divide=a/b
+print(divide)
---
+++ lesser.py
@@ -0,0 +1,16 @@
+#program to check lesser of 2 numbers
+#input a from keyboard
+a=int(input("enter value of a"))
+#input b from keyboard
+b=int(input("enter value of b"))
+#print value of a
+print("A:",a)
+#print value of b
+print("B:",b)
+
+if(a<b):
+	print(a,"is lesser")
+else:
+	print(b,"is lesser")
+
+
