d8f3702c84018b2e81fbd180dcdb19c81e5358e0
pythonsai10 <43162447+pythonsai10@users.noreply.github.com>
2018-09-18 10:22:31 +0530

    Add files via upload

---
+++ bunking.py
@@ -0,0 +1,9 @@
+while True:
+	i=input("enter'n'to quite")
+	if(i=='n'):
+		print("Bye!")
+		exit()
+
+	else:
+		print("try again")
+		
---
+++ table.py
@@ -0,0 +1,8 @@
+#aqsk the user for a number and 
+#print the table of that number till 20
+a=int(input("give a number"))
+b=1
+while(b<=20):
+	n=a*b
+	print(a,"*",b,"=",n)
+	b=b+1
