8946459629cb3bb37fcdc2ea763d0fb2cc006c9e
pythonsai10 <43162447+pythonsai10@users.noreply.github.com>
2018-09-24 14:18:38 +0530

    Update try.py

--- try.py
+++ try.py
@@ -5,5 +5,12 @@ while True:
 		exit()
 
 	else:
-		print("try again")	
+		print("try again")
+output
+cl314@soetcse:~/saikiran$ python3 try.py
+enter'n'to quite n
+try again
+enter'n'to quiten
+bye
+
 		
