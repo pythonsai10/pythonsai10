c2a7ccd4866bbf9127f26bb45ec68dc06220d0b2
pythonsai10 <43162447+pythonsai10@users.noreply.github.com>
2018-10-05 16:32:37 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,12 @@
+import random
+d={1:'R',2:'P',3:'S'}
+c=d[random.randint(1,3)]
+while(True):
+	p=input("enter'R','P','S'")
+	if(c==p):
+ 		print("tie")
+	elif(c=='R'and p=='S' or c=='P'and p=='R' or c=='S'and p=='P'):
+		print("computer wins")
+	else:
+		print("player won")
+		break
