f6c3eae69838e14367994bc273fdd402e6c1a5ed
pythonsai10 <43162447+pythonsai10@users.noreply.github.com>
2018-10-29 11:42:55 +0530

    Add files via upload

---
+++ amail.py
@@ -0,0 +1,24 @@
+import smtplib
+import getpass
+
+myemail=input("your email id : ")
+password=getpass.getpass()
+recemail=input("Reciever's email id :")
+
+#create SMTP session
+s=smtplib.SMTP('smtp.gmail.com',587)
+
+#start TLS for security
+s.starttls()
+
+#authentication
+s.login(myemail,password)
+
+#message to be sent
+message="Hey bro9ther"
+
+#sending the mail
+s.sendmail(myemail,recemail,message)
+
+#terminating the sesion
+s.quit()
