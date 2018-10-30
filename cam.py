4c08951c97fde8403ac10e69efd6e200edc0ee07
pythonsai10 <43162447+pythonsai10@users.noreply.github.com>
2018-10-30 07:31:09 +0530

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,5 +1,13 @@
+PROGRAM 
 import os
 i=0
-while(i<=10):
+while(i<=5):
     os.system("fswebcam -F 4 --fps 30 -r 1200*1000 /home/pi/group2/"+str(i)+".jpg")
     i=i+1
+    print("pic taken")
+OUTPUT
+pic taken
+pic taken
+pic taken 
+pic taken
+pic taken
